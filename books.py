17eacb13415d592d6abe59bcd1675d3b04e032aa
Mona Lisa <147712158+thusiru@users.noreply.github.com>
2024-09-09 22:27:22 +0530

    initialization of main.py, library.py and books.py

---
+++ books.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, title, author):
+        ...
+
+    def borrow():
+        """To take a book or other library material for a specified period, typically with the agreement that it will be returned by a set date."""
+
+    def restore():
+        """To give back a borrowed book or material to the library, ensuring it's no longer registered under the borrower’s name."""
+
+    def search():
+        """To look up books or resources in the library’s database or catalog by title, author, subject, or keyword to locate specific items."""
