8833db99902b165d72883657d64177709ec72764
Mona Lisa <147712158+thusiru@users.noreply.github.com>
2024-09-12 16:09:53 +0530

    Add login function and logged_menu function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from books import Books
 from consolemenu import ConsoleMenu
-from consolemenu.items import FunctionItem
+from consolemenu.items import FunctionItem, SubmenuItem, SelectionItem
 from getpass import getpass
 from print_color import print as printf
 from time import sleep
@@ -9,26 +9,79 @@ from user import User
 
 def main():
     """main function"""
-    menu = ConsoleMenu("Main menu", "Welcome to Library!")
+    main_menu()
 
-    function_login = FunctionItem("Login", login)
+
+def main_menu():
+    """Main menu"""
+    # Create main menu
+    menu = ConsoleMenu("Main menu", "Welcome to Library!", clear_screen=False)
+
+    # Create main menu items
+    function_login = FunctionItem("Login", login, [menu])
     function_register = FunctionItem("Register", register)
     function_search = FunctionItem("Book search", search)
 
+    # Append main menu items
     menu.append_item(function_login)
     menu.append_item(function_register)
     menu.append_item(function_search)
 
+    # Show main menu
     menu.show()
 
 
-def route(function):
-    if function == "login":
-        login()
-    if function == "register":
-        register()
-    if function == "search":
-        search()
+def logged_menu(user, main_menu):
+    """Logged in menu"""
+    # Pause main menu
+    main_menu.pause()
+
+    # Create menu
+    menu = ConsoleMenu(
+        "Main menu", f"Hi {user}!", exit_option_text="Logout", clear_screen=False
+    )
+
+    # Menu item 1
+    function_search = FunctionItem("Book search", search)
+
+    # Menu item 2
+    my_books_menu = ConsoleMenu("My books", exit_option_text="Back", clear_screen=False)
+    my_books = SubmenuItem("My books", my_books_menu, menu=menu)
+
+    # Submenu items
+
+    # Menu item 3
+    my_account_menu = ConsoleMenu(
+        "My account", exit_option_text="Back", clear_screen=False
+    )
+    my_account = SubmenuItem("My account", my_account_menu, menu=menu)
+
+    # Submenu items
+    function_username = FunctionItem("Change Username", change_username, [user])
+    function_password = FunctionItem("Change Password", change_password, [user])
+
+    # Append menus and submenus
+    menu.append_item(function_search)
+    menu.append_item(my_books)
+    menu.append_item(my_account)
+    my_account_menu.append_item(function_username)
+    my_account_menu.append_item(function_password)
+
+    # Show menu
+    menu.show()
+
+    # Resume main menu
+    main_menu.resume()
+
+
+def change_username(user):
+    user.change_username()
+    continues()
+
+
+def change_password(user):
+    print(user.change_password())
+    continues
 
 
 def search():
@@ -58,11 +111,14 @@ def search():
     continues()
 
 
-def login():
+def login(menu):
     """User login"""
     user = User.login()
-    print(user)
-    continues()
+    if user:
+        continues()
+        logged_menu(user, menu)
+    else:
+        continues()
 
 
 def register():
