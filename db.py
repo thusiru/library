eece55f0e26e8d80db0fa714e9bff9941a96ea57
Mona Lisa <147712158+thusiru@users.noreply.github.com>
2024-09-12 16:14:28 +0530

    Change database connection from a function to class for context management

--- db.py
+++ db.py
@@ -5,13 +5,18 @@ import threading
 thread_local = threading.local()
 
 
+class DatabaseConnection:
+    def __enter__(self):
+        if not hasattr(thread_local, "connection"):
+            thread_local.connection = sqlite3.connect("library.db")
+        self.connection = thread_local.connection
+        return self.connection
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if hasattr(thread_local, "connection"):
+            thread_local.connection.close()
+            del thread_local.connection
+
+
 def get_db_connection():
-    if not hasattr(thread_local, "connection"):
-        thread_local.connection = sqlite3.connect("library.db")
-    return thread_local.connection
-
-
-def close_db_connection():
-    if hasattr(thread_local, "connection"):
-        thread_local.connection.close()
-        del thread_local.connection
+    return DatabaseConnection()
